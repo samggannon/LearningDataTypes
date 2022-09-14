0a45aa114256b13911863af4b5d56a9e5f33488e
Samuel Gannon <samgannon@rocketmail.com>
2022-09-14 10:21:35 -0500

    Add project files.

---
+++ LearningDataTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿// variables
+
+int index = 1;
+string name = "Sam";
+char intial = 'S';
+int year = 2022;
+decimal height = 1.85m;
+bool iLoveToCode = true;
+
+String MyParagraph = $@"These are the most common data types:
+{index++} - string, example: {name}
+{index++} - char, example: {intial}
+{index++} - int, example: {year}
+{index++}  - decimal, example: {height}
+{index++}  - bool, example: {iLoveToCode}";
+
+Console.WriteLine(MyParagraph);
+
+Console.ReadLine();
